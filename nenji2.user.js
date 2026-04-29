3f3dac22b53ab19e686ea5bc7020c9eb8b4e9279
midoriI2026 <106957602+midoriI2026@users.noreply.github.com>
2026-04-29 13:17:15 +0900

    Add user script for 年次CopyPaste functionality

---
+++ nenji2.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         ★年次CopyPaste（再利用ボタン方式・安定版）
+// @namespace    http://tampermonkey.net/
+// @version      8.0
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBRecord&did=35*
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBForm&did=35*
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?Page=DBSearchResult&DID=35*
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/midoriI2026/nenji2/main/nenji2.js
+// @grant        none
+// ==/UserScript==
